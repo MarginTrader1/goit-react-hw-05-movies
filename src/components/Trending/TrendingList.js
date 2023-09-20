e4d65c99896cdebf38f43520b45f5d6842faaff5
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-09-20 14:47:59 +0300

    Update TrendingList.js

--- src/components/Trending/TrendingList.js
+++ src/components/Trending/TrendingList.js
@@ -2,9 +2,17 @@ import { TrendingItem } from 'components/TrendingItem/TrendingItem';
 import { List } from './TrendingList.styled';
 
 export const TrendingList = ({ movies }) => {
+<<<<<<< Updated upstream:src/components/Trending/TrendingList.js
   
   console.log(`Это консоль в TrendingList ${movies}`);
 
+=======
+  console.log(movies);
+
+  const [trendMovies] = movies;
+  const trendId = trendMovies.id
+  
+>>>>>>> Stashed changes:src/components/Trending/Trending.js
   return (
     <List>
       {movies.map(item => (
